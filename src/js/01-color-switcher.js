f4664e2454a8764b33a7aef07a01678a1b91a3f3
Vadym Prokhorenko <prokhorenko.vadim.98@gmail.com>
2023-05-14 20:28:00 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
-
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+let interval;
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    interval = setInterval(setBackgroundColor, 1000)
+})
+stopBtn.addEventListener("click", () => {
+    startBtn.disabled = false;
+    if (interval) clearInterval(interval);
+})
+function setBackgroundColor() {
+    document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
