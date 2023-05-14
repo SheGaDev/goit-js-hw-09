ac78f867fc76227f92261a246b88ccd4a49ad616
Vadym Prokhorenko <prokhorenko.vadim.98@gmail.com>
2023-05-14 22:32:51 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector(".form");
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      } else {
+        rej(`❌ Rejected promise ${position} in ${delay}ms`)
+      }
+    }, delay)
+  })
 }
+async function event(e) {
+  e.preventDefault();
+  const data = {};
+  for (const [name, value] of Object.entries(e.currentTarget.elements))
+    data[name] = +value.value;
+  for (let i = 0; i < data.amount; i++) {
+    createPromise(i, data.delay).then(Notify.success).catch(Notify.failure);
+    data.delay += data.step;
+  }
+  e.currentTarget.reset();
+}
+form.addEventListener("submit", event);
